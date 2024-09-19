f45712bf89590acdf08926f185eca3e2d6d1f395
Alexandr Kurganov <alexandr.kurs.it@gmail.com>
2024-09-19 18:46:10 +0600

    Create App.tsx and AddTaskForm.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import AddTaskForm from "./Components/AddTaskForm.tsx";
+
+const App= () => {
+
+  return (
+      <div className="form">
+            <AddTaskForm />
+      </div>
+  )
+};
+
+export default App;
---
+++ src/Components/AddTaskForm.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+
+interface Task {
+    id: string;
+    text: string;
+    completed: boolean;
+}
+
+const AddTaskForm = () => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [newTask, setNewTask] = useState ('');
+
+    const addTask = () => {
+        if (newTask !== '') {
+            const newId = crypto.randomUUID ();
+            const newTaskItem: Task = {
+                id: newId,
+                text: newTask,
+                completed: false,
+            };
+            setTasks([...tasks, newTaskItem]);
+            setNewTask('');
+        }
+    };
+
+    const removeTask = (id: string) => {
+        const updatedTodos = tasks.filter((task) => task.id !== id);
+        setTasks(updatedTodos);
+    };
+
+    const toggleComplete = (id: string) => {
+        const updatedTodos = tasks.map((task) => {
+            if (task.id === id) {
+                return { ...task, completed: !task.completed };
+            }
+            return task;
+        });
+        setTasks(updatedTodos);
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+            />
+            <button onClick={addTask}>Add</button>
+            <ul className="list">
+                {tasks.map((task) => (
+                    <li className="task" key={task.id}>
+                        <input
+                            type="checkbox"
+                            checked={task.completed}
+                            onChange={() => toggleComplete(task.id)}
+                        />
+                        <span style={{textDecoration: task.completed ? 'line-through' : 'none'}}>
+                            {task.text}
+                        </span>
+                        <button onClick={() => removeTask(task.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default AddTaskForm;
